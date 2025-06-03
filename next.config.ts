311cf41f22a25c4b4c6573d9ca1aca8d32e8bf77
Kumbha2Nagarjuna <nagarjunakumbhacse@gmail.com>
2025-06-02 19:04:45 -0500

    first code push

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export',
+  basePath: '/next-report-app',
+  assetPrefix: '/next-report-app/',
+};
+
+export default nextConfig;
